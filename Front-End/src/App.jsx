15d54ebca51dc7a037a488a4ec5e81e38b1d4947
Aozora Asuka <azdin1411@gmail.com>
2025-02-24 09:04:45 +0700

    edit router

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -11,7 +11,10 @@ function App() {
     <>
       {!location.pathname.startsWith("/admin") ? <Header /> : null}
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<HomePage />}>
+          <Route path="signin" element={<UserAuthForm type="sign-in" />} />
+          <Route path="signup" element={<UserAuthForm type="sign-up" />} />
+        </Route>
         <Route path="/products/:productId" element={<ProductDetailPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
